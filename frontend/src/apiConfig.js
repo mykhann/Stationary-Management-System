66b6b52cf9c7501a1bc946a479f32e9a0288e4ad
yousuf khann <mykhann10@gmail.com>
2026-01-21 18:02:10 +0500

    fixed the url in api config too

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://statiocore-stationary-management-system.onrender.com/";
+const BASE_URL = "https://statiocore-stationary-management-system.onrender.com";
 
 export default BASE_URL;
