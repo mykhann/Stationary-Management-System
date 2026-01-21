c43650e27b1a3acab3d75310d043346e64d71ba0
yousuf khann <mykhann10@gmail.com>
2026-01-21 18:00:58 +0500

    fixed the url on index

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'https://statiocore-stationary-management-system.onrender.com/',
+    origin: 'https://statiocore-stationary-management-system.onrender.com',
     credentials: true
 }
 app.use(cors(corsOptions))
